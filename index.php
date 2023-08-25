b1e327390f39b9abba347030f3af1499bb3ca4ed
jeffsasse <jeffsasse0603@hotmail.com>
2023-08-25 14:36:03 -0400

    add index with guide

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+// PHP Rest API codebase with Thermostat
+
+// My primary focus has been on constructing the REST API using PHP. 
+// During the development process, I worked on establishing connections with thermostats via APIs. 
+// These APIs were designed to facilitate various functionalities, including reading thermostat information, 
+// creating new thermostats, and editing temperature settings.
+
+// For detailed insights into the thermostat information and functionalities, 
+// you can refer to [this site](https://www.britannica.com/technology/thermostat)
+?>
